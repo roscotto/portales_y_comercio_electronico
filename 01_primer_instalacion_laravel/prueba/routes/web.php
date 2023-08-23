c7a0e7b8561f557b6496ff7d32722a15fd890c23
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-08-23 13:34:16 -0300

    comentarios

--- 01_primer_instalacion_laravel/prueba/routes/web.php
+++ 01_primer_instalacion_laravel/prueba/routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// En este archivo definimos las rutas de nuestra aplicación web
+// Cada ruta es la combinacion de:
+// a. El verbo/ método HTTP (get, post, put, patch, delete). Se define segun el método de Route al que llamemos.
+// b. La URL. Que siempre empieza a partir de la carpeta public. / este es el primer parametro de Route::get
+// c. Una acción. La función anónima que se ejecutará cuando se acceda a la ruta. (segundo parámetro)
+// Puede ser una función anónima o un controlador. En este caso es una función anónima.
 Route::get('/', function () {
+    // Todas las acciones de una ruta DEBEN retornar una respuesta, que puede ser una vista/template de HTML, 
+    // o alguna otra cosa como un objeto JSON.
+    // Si queremos retornar vistas, podemos usar la función view() que nos devuelve una vista.
+    // Este método recibe como argumento el nombre de la vista que queremos retornar. sin la extensión .php ni .blade.php
+    // Las vistas son templates de HTML que se encuentran en la carpeta resources/views
     return view('welcome');
+});
+
+// Agregamos otras vistas
+
+Route::get('/quienes-somos', function () {
+    return view('about');
 });
