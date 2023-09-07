86a7dd9a075e83b307d9082f1a89bca9b56535ac
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-07 11:03:24 -0300

    creacion de modelo de Eloquent - Movie

---
+++ 01_primer_instalacion_laravel/prueba/app/Models/Movie.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    // use HasFactory;
+
+    // indicamos el nombre de la tabla (es opcional, ya que por defecto Laravel asume que el nombre de la tabla es el plural del nombre del modelo)
+    protected string $table = 'movies';
+
+    // Indicamos el nombre de la columna que es la clave primaria (es opcional, ya que por defecto Laravel asume que la clave primaria es la columna id) En este caso, es obligatorio ponerlo ya que la clave primaria es movie_id.
+    protected string $primaryKey = 'movie_id';
+}
